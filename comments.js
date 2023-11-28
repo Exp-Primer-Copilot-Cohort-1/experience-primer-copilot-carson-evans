781413e93c2ea081133d6d0de450c8ec9abb07a1
Carson Evans <carsonevans@outlook.com>
2023-11-28 18:04:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,67 +1,68 @@
-// Create web server to handle comments
-// GET, POST, PUT, DELETE
+// Create web server application with react.js
 
-// Import express
-const express = require('express');
-const commentsRouter = express.Router();
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+// Import modules
+import React, { Component } from 'react';
+import * as service from '../services/posts';
 
-// Import body-parser
-const bodyParser = require('body-parser');
-commentsRouter.use(bodyParser.json());
+// Create comments component
+class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+      comment: '',
+    };
+  }
 
-// Get all comments
-commentsRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Comment WHERE is_deleted = 0 AND issue_id = $issueId', {
-        $issueId: req.issue.id
-    }, (err, comments) => {
-        if (err) {
-            next(err);
-        } else {
-            res.status(200).json({comments: comments});
-        }
-    });
-});
+  // Create componentDidMount function
+  componentDidMount() {
+    this.fetchComments();
+  }
 
-// Check if comment with id exists
-commentsRouter.param('commentId', (req, res, next, commentId) => {
-    db.get('SELECT * FROM Comment WHERE id = $commentId AND issue_id = $issueId', {
-        $commentId: commentId,
-        $issueId: req.issue.id
-    }, (err, comment) => {
-        if (err) {
-            next(err);
-        } else if (comment) {
-            req.comment = comment;
-            next();
-        } else {
-            res.status(404).send();
-        }
-    });
-});
+  // Create fetchComments function
+  fetchComments = async () => {
+    const { postId } = this.props;
+    const comments = await service.fetchComments(postId);
+    this.setState({ comments });
+  };
 
-// Get a comment with id
-commentsRouter.get('/:commentId', (req, res, next) => {
-    res.status(200).json({comment: req.comment});
-});
+  // Create handleCommentChange function
+  handleCommentChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
 
-// Create a new comment
-commentsRouter.post('/', (req, res, next) => {
-    const content = req.body.comment.content;
-    const isDeleted = req.body.comment.isDeleted === 0 ? 0 : 1;
-    const issueId = req.issue.id;
-    if (!content) {
-        res.status(400).send();
-    } else {
-        db.run('INSERT INTO Comment (content, is_deleted, issue_id) VALUES ($content, $isDeleted, $issueId)', {
-            $content: content,
-            $isDeleted: isDeleted,
-            $issueId: issueId
-        }, function(err) {
-            if (err) {
-                next(err);
-            } else {
-                db.get('SELECT * FROM Comment WHERE id = $commentId', {
-                    $commentId: this.lastID
-                }, (err, comment
+  // Create handleCommentSubmit function
+  handleCommentSubmit = async () => {
+    const { postId } = this.props;
+    const { comment } = this.state;
+    await service.createComment(postId, comment);
+    this.setState({ comment: '' });
+    this.fetchComments();
+  };
+
+  // Create render function
+  render() {
+    const { comments, comment } = this.state;
+    return (
+      <div>
+        <h2>Comments</h2>
+        <ul>
+          {comments.map(({ id, body }) => (
+            <li key={id}>{body}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={comment}
+          onChange={this.handleCommentChange}
+        />
+        <button type="button" onClick={this.handleCommentSubmit}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+// Export default
+export default Comments;
